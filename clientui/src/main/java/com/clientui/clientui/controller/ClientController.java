14f30643fa47c7315c413abe9667df36944d4df1
laflosh <l_florent_l@hotmail.fr>
2025-03-05 14:17:44 +0100

    clientui: add one product page

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.clientui.clientui.beans.ProductBean;
@@ -26,5 +27,20 @@ public class ClientController {
 		return"Accueil";
 
 	}
+	
+	@RequestMapping("/details-produit/{id}")
+	public String ficheProduit(@PathVariable int id, Model model) {
+		
+		ProductBean produit = productProxy.recupererUnProduit(id);
+		
+		System.out.println(produit);
+		
+		model.addAttribute("produit", produit);
+		
+		return"FicheProduit";
+		
+	}
+	
+	//@RequestMapping("/details-produit/commander-produit/{id}")
 
 }
