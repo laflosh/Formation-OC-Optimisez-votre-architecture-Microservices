bf09592ea5639e9ef159af9a2c18d64d563cfc32
laflosh <l_florent_l@hotmail.fr>
2025-03-07 16:19:05 +0100

    config eureka for all services

--- microservice-commandes/src/main/java/com/mcommandes/model/Commande.java
+++ microservice-commandes/src/main/java/com/mcommandes/model/Commande.java
@@ -1,11 +1,12 @@
 package com.mcommandes.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Commande {
